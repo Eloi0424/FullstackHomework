78203258b77c9a2452a57827b7e8b9862b5da3ce
Eloi0424 <beichu0424@qq.com>
2023-10-22 21:10:17 +0800

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   part1/src/App.js
    Note:
        part 1 compelete

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,46 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  const [vote = new Array(anecdotes.length),setVote] = useState(new Array(anecdotes.length).fill(0))
+
+
+
+  let genButton = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+
+
+  let voteButton = () => {
+    const voteNew = [...vote]
+    voteNew[selected] += 1
+    setVote(voteNew)
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <h3>Today anecdote</h3>
+      <p>{anecdotes[selected]}</p>
+      <p>Has {vote[selected]} vote</p>
+      <div>
+        <button onClick={voteButton}>vote</button>
+        <button onClick={genButton}>Gen a anecdote</button>
+      </div>
+      <h3>Anecdote with most votes</h3>
+      <p>{anecdotes[vote.indexOf(Math.max(...vote))]}</p>
+      <p>Has {Math.max(...vote)} vote</p>
     </div>
   )
 }
